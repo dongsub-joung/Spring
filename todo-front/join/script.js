4db3f7376f10358ea41139c366ee8605a4b9cad9
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-28 18:05:50 +0900

    moto

--- todo-front/join/script.js
+++ todo-front/join/script.js
@@ -7,10 +7,30 @@ document.getElementById('joinForm').addEventListener('submit', function(event) {
   
     // Perform basic validation (replace this with server-side validation in a real application)
     if (newUsername.trim() !== '' && newPassword.trim() !== '') {
+
+      makePostRequest(newUsername, newPassword);
+      
       document.getElementById('joinMessage').textContent = 'Registration successful!';
       // Perform registration logic here (e.g., store the new credentials)
     } else {
       document.getElementById('joinMessage').textContent = 'Please enter both username and password.';
     }
   });
+
+  async function makePostRequest(userId, userPW) {
+    try {
+      await fetch("https://todo-api.ngrok.app/api/join", {
+        method: "POST",
+        body: JSON.stringify({
+          _id: userId,
+          _pw: userPW,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      });  
+    } catch (error) {
+      throw new Error('There was an error with the POST request: ' + error.message);
+    }
+  }
   
--- todo-front/login/script.js
+++ todo-front/login/script.js
@@ -1,16 +1,36 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    
-    // Get the input values
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
-    // Perform basic validation (replace this with server-side validation in a real application)
-    if (username === 'username' && password === 'password') {
-      document.getElementById('loginMessage').textContent = 'Login successful!';
-      // Redirect to another page or perform necessary actions upon successful login
-    } else {
-      document.getElementById('loginMessage').textContent = 'Invalid username or password. Please try again.';
-    }
-  });
-  
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  // Get the input values
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  let data= makePostRequest(username, password);
+  console.log(data);
+
+  // Perform basic validation (replace this with server-side validation in a real application)
+  if (username === 'username' && password === 'password') {
+    document.getElementById('loginMessage').textContent = 'Login successful!';
+    // Redirect to another page or perform necessary actions upon successful login
+  } else {
+    document.getElementById('loginMessage').textContent = 'Invalid username or password. Please try again.';
+  }
+});
+
+
+async function makePostRequest(userId, userPW) {
+  try {
+    await fetch("https://todo-api.ngrok.app/api/login", {
+      method: "POST",
+      body: JSON.stringify({
+        _id: userId,
+        _pw: userPW,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    });  
+  } catch (error) {
+    throw new Error('There was an error with the POST request: ' + error.message);
+  }
+}
